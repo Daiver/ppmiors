8733096cd911f38f35975c9724c3a77194ae87fa
daiver <ra22341@ya.ru>
2015-10-18 14:45:49 +0300

    update API change functions names

--- examples/read_p6_write_p3.rs
+++
@@ -1,9 +0,0 @@
-#[macro_use] extern crate rustonum;
-extern crate ppmiors;
-
-fn main()
-{
-    let res = ppmiors::read_ppm_p6("/home/daiver/pstorage/AlexF.ppm");
-    println!("saving....");
-    ppmiors::save_ppm_p3(&res[0], &res[1], &res[2], "tmp.pgm");
-}
--- examples/read_p6_write_p6.rs
+++ examples/read_p6_write_p6.rs
@@ -5,5 +5,5 @@ fn main()
 {
     let res = ppmiors::read_ppm_p6("/home/daiver/pstorage/AlexF.ppm");
     println!("saving....");
-    ppmiors::save_ppm_p6(&res[0], &res[1], &res[2], "tmp.pgm");
+    ppmiors::write_ppm_p6(&res, "tmp.pgm");
 }
